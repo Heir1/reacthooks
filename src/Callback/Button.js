62c67728a91e6ac8ac5d1649e34f7c84dcb3dd47
heir1 <nkeleheir@gmail.com>
2021-12-30 04:09:12 +0100

    correction sur le margin top de bouttons

--- src/Callback/Button.js
+++ src/Callback/Button.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Button({handleClick, bgColor, increment, children}) {
-    const bg = `btn btn-${bgColor}`;
+    const bg = `btn btn-${bgColor} mt-3`;
     console.log(`Button ${children}`)
     return <button className={bg} onClick={() => handleClick(increment)}>+ {increment} %</button>
 }
